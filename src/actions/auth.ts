8ddbab24177d440c64025fafe56f43e218306ef6
Lucas Zebre <yakirisk@gmail.com>
2024-10-07 20:27:49 +0200

    fix : change the env var URL

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,21 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use server'
+"use server";
 
-
-import betaseries from '@api/betaseries';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-process.env
+import betaseries from "@api/betaseries";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+process.env;
 export const auth = async () => {
-    const cookieStore = cookies()
-    betaseries.auth(process.env.NEXT_PUBLIC_BETASERIES_KEY as string);
-    const data =  await betaseries.postMembersOauth({client_id:process.env.NEXT_PUBLIC_BETASERIES_KEY}) as any
-    
-    cookieStore.set('redirect_uri','http://127.0.0.1:3000/')
-    cookieStore.set('client_id',process.env.NEXT_PUBLIC_BETASERIES_KEY as string)
-    cookieStore.set('client_secret',process.env.NEXT_PUBLIC_BETASERIES_CLIENT_SECRET as string)
+  const cookieStore = cookies();
+  betaseries.auth(process.env.NEXT_PUBLIC_BETASERIES_KEY as string);
+  const data = (await betaseries.postMembersOauth({
+    client_id: process.env.NEXT_PUBLIC_BETASERIES_KEY,
+  })) as any;
 
-    redirect(`${data.data.oauth.callback}?redirect_uri=http://127.0.0.1:3000/&&client_id=${process.env.NEXT_PUBLIC_BETASERIES_KEY}`)
-    
+  cookieStore.set("redirect_uri", process.env.NEXT_PUBLIC_URL as string);
+  cookieStore.set(
+    "client_id",
+    process.env.NEXT_PUBLIC_BETASERIES_KEY as string
+  );
+  cookieStore.set(
+    "client_secret",
+    process.env.NEXT_PUBLIC_BETASERIES_CLIENT_SECRET as string
+  );
 
+  redirect(
+    `${data.data.oauth.callback}?redirect_uri=${process.env.NEXT_PUBLIC_URL}&&client_id=${process.env.NEXT_PUBLIC_BETASERIES_KEY}`
+  );
 };
